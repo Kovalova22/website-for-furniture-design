1f353a2f6a9c00173a23611bcb17a51d8f9b897f
Iryna Kovalova <ikovalova333@gmail.com>
2021-05-04 20:05:04 +0300

    add header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 
-import styles from './header.module.css';
+import styles from './header.module.scss';
 import Cart from '../../cart.svg';
 
 export default function Header() {
   return (
     <header>
-      <div>cleo</div>
-      <div>
-        <a href="/">Facebook</a>
-        <a href="/">Instagram</a>
-        <a href="/">Twitter</a>
+      <div className={styles.logo}>cleo</div>
+      <div className={styles.social}>
+        <span>
+          <a href="/">Facebook</a>
+        </span>
+        <span>
+          {' '}
+          <a href="/">Instagram</a>
+        </span>
+        <span>
+          <a href="/">Twitter</a>
+        </span>
       </div>
-      <div className={styles.menu_container}>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div>
-        <img src={Cart} alt="cart"></img>
+      <div className={styles.right}>
+        <div className={styles.right__menu_container}>
+          <div className={styles.right__menu_container__sandwich}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className={styles.right__svg}>
+          <img src={Cart} alt="cart"></img>
+        </div>
       </div>
     </header>
   );
---
+++ src/components/main/main.tsx
@@ -0,0 +1,7 @@
+export default function MainPicture() {
+  return (
+    <picture>
+      <source />
+    </picture>
+  );
+}
