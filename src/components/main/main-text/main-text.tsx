badb3a5db25a005be26b97f3af2dfca3d9fc728f
Iryna Kovalova <ikovalova333@gmail.com>
2021-05-04 20:50:16 +0300

    style main

--- src/components/main/main-text/main-text.tsx
+++ src/components/main/main-text/main-text.tsx
@@ -5,12 +5,12 @@ import styles from './main-text.module.scss';
 export default function MainText() {
   return (
     <div className={styles.container}>
-      <h1>Cosy design</h1>
-      <p>
+      <h1 className={styles.container__heading}>Cosy design</h1>
+      <p className={styles.container__text}>
         Create comfort in your living room with our new stylish collection of furniture and
         accessories made of high quality materials.
       </p>
-      <button>See collection</button>
+      <button className={styles.container__main_Btn}>See collection</button>
     </div>
   );
 }
